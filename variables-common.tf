8a292cd54262efbe7c2e47e73ec4da15754b4951
bence.bano <{ID}+{username}@users.noreply.github.com>
2022-10-03 02:21:14 +0200

    update

--- variables-common.tf
+++ variables-common.tf
@@ -3,7 +3,7 @@ variable "subscription_id" {
   description = "ID of the Subscription"
   type = any
   validation {
-    condition = can(regex("\\b[0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b", var.SubscriptionID))
+    condition = can(regex("\\b[0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b", var.subscription_id))
     error_message = "Must be a valid subscription id. Ex: 9e4e50cf-5a4a-4deb-a466-9086cd9e365b."
   }
 }
